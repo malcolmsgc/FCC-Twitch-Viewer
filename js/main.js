b49baa1b1de272c4eeea4ccfb0451f78a2a40c8d
Malcolm Cumming <bemjogar@gmail.com>
2017-03-10 22:24:19 +0000

    JS file with example api call urls

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+/*
+EXAMPLE CALLS
+
+FOR ON/OFFLINE: 
+https://wind-bow.gomix.me/twitch-api/streams/freecodecamp
+
+FOR CHANNEL INFO AND ASSETS
+https://wind-bow.gomix.me/twitch-api/channels/freecodecamp
+*/
