84d76c43a92cf6699e2cf659cc2720b610de7375
Malcolm Cumming <bemjogar@gmail.com>
2017-03-12 21:33:07 +0000

    beginning of fetch stream status

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ https://wind-bow.gomix.me/twitch-api/streams/freecodecamp
 FOR CHANNEL INFO AND ASSETS
 https://wind-bow.gomix.me/twitch-api/channels/freecodecamp
 */
+
+function fetchStatus (streams = ['freecodecamp']) {
+    const statusArray = streams.map(
+        //callback
+        function(stream) {
+        const statusResult = fetch('https://wind-bow.gomix.me/twitch-api/streams/' + stream);
+        statusResult
+            .then (result => result.json())
+            .then (json => {console.log(json)})
+            .then (jsonObj => {console.log(stream); return {stream, online: jsonObj.stream != null}})
+            .catch(err => (console.log(err)));
+    });
+    return statusArray;
+} 
