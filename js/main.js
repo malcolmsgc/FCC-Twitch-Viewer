7c3d224096e141f1d06ff45005cd7fcb2117541e
Malcolm Cumming <bemjogar@gmail.com>
2017-03-12 21:48:20 +0000

    Small refactor on fetch API call

--- js/main.js
+++ js/main.js
@@ -13,12 +13,14 @@ function fetchStatus (streams = ['freecodecamp']) {
     const statusArray = streams.map(
         //callback
         function(stream) {
-        const statusResult = fetch('https://wind-bow.gomix.me/twitch-api/streams/' + stream);
-        statusResult
-            .then (result => result.json())
-            .then (json => {console.log(json)})
-            .then (jsonObj => {console.log(stream); return {stream, online: jsonObj.stream != null}})
-            .catch(err => (console.log(err)));
+            const statusResult = fetch('https://wind-bow.gomix.me/twitch-api/streams/' + stream);
+            statusResult
+                .then (result => result.json())
+                //.then (json => {console.log(json)})
+                .then (jsonObj => {console.log(jsonObj);
+                                    console.log(`{${stream}, online: ${jsonObj['stream'] !== null}}`); 
+                                    return {stream, online: jsonObj['stream'] !== null}})
+                .catch(err => (console.log(err)));
     });
     return statusArray;
 } 
