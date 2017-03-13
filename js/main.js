dfa979ed40cda786d5532482a5f7dda0b7ef3847
Malcolm Cumming <bemjogar@gmail.com>
2017-03-13 23:34:55 +0000

    Refactor fetchStatus to fetch >1 time

--- js/main.js
+++ js/main.js
@@ -3,24 +3,25 @@
 EXAMPLE CALLS
 
 FOR ON/OFFLINE: 
-https://wind-bow.gomix.me/twitch-api/streams/freecodecamp
+https://wind-bow.glitch.me/twitch-api/streams/freecodecamp
 
 FOR CHANNEL INFO AND ASSETS
-https://wind-bow.gomix.me/twitch-api/channels/freecodecamp
+https://wind-bow.glitch.me/twitch-api/channels/freecodecamp
 */
 
 function fetchStatus (streams = ['freecodecamp']) {
-    const statusArray = streams.map(
-        //callback
-        function(stream) {
-            const statusResult = fetch('https://wind-bow.gomix.me/twitch-api/streams/' + stream);
-            statusResult
-                .then (result => result.json())
-                //.then (json => {console.log(json)})
-                .then (jsonObj => {console.log(jsonObj);
-                                    console.log(`{${stream}, online: ${jsonObj['stream'] !== null}}`); 
-                                    return {stream, online: jsonObj['stream'] !== null}})
-                .catch(err => (console.log(err)));
-    });
-    return statusArray;
+    Promise
+        .all( streams.map( stream => {
+            const   statusResult = fetch('https://wind-bow.glitch.me/twitch-api/streams/' + stream);
+                    statusResult
+                        .then (result => result.json())
+                        .then (jsonObj => { console.log(jsonObj);
+                            console.log(`{${stream}, online: ${jsonObj['stream'] !== null}}`); 
+                            return {stream, online: jsonObj['stream'] !== null}})
+                        .catch(err => (console.log(err)));
+    }
+        ))
+        .then( (resultsArray) => { console.log(resultsArray) } )
+    
+
 } 
