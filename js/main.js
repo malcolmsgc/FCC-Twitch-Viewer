3c7d0c1286991bbae5860c983614dc687d6a73cd
Malcolm Cumming <bemjogar@gmail.com>
2017-03-15 01:05:49 +0000

    Promise returns array w off/online status

--- js/main.js
+++ js/main.js
@@ -9,19 +9,17 @@ FOR CHANNEL INFO AND ASSETS
 https://wind-bow.glitch.me/twitch-api/channels/freecodecamp
 */
 
-function fetchStatus (streams = ['freecodecamp']) {
-    Promise.all( streams.map( stream => fetch('https://wind-bow.glitch.me/twitch-api/streams/' + stream) ) )
-        .then ( (responses) => 
-        Promise.all( responses.map(response => response.json()) )
-        )
-        .then ( (json) => {console.log(json)} );
-    
+function fetchStatus (...streams) {
+    return  Promise.all( streams.map( stream => fetch('https://wind-bow.glitch.me/twitch-api/streams/' + stream) ) )
+            .then ( responses => Promise.all( responses.map(response => response.json()) ) )
+            .then ( json => 
+                json.map( (obj, i) => { return {stream: streams[i], online: obj['stream'] !== null} } ) )
+            .then ( resultArray => {
+                console.log(resultArray); 
+                return resultArray
+            } )
+            .catch ( err => { console.log(err); } );
+} // returns promise object which can be used within another promise chain or evaluate by looking at PromiseValue
 
-} 
+fetchStatus('freecodecamp', 'testchannel');
 
-
-                       /* .then (result => result.json())
-                        .then (jsonObj => { console.log(jsonObj);
-                            console.log(`{${stream}, online: ${jsonObj['stream'] !== null}}`); 
-                            return {stream, online: jsonObj['stream'] !== null}})
-                        .catch(err => (console.log(err)));*/
