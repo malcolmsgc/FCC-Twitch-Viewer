baf9d06d51b78d2be5c30b5263b95f3114a5647f
Malcolm Cumming <bemjogar@gmail.com>
2017-03-16 00:10:32 +0000

    Error handling in fetchStatus

--- js/main.js
+++ js/main.js
@@ -10,6 +10,17 @@ https://wind-bow.glitch.me/twitch-api/channels/freecodecamp
 */
 
 function fetchStatus (...streams) {
+    //validate args
+    try {
+        const errorArray = [];
+        streams.map( (stream, i) => { if (typeof stream !== "string") errorArray.push(i); } );
+        if (errorArray.length > 0) throw new Error (
+            `fetchStatus called with ${errorArray.length} arguments of unsupported type in index ${errorArray}.
+Arguments should be strings`);
+    }
+    catch (err) {
+        console.error(err);
+    }
     return  Promise.all( streams.map( stream => fetch('https://wind-bow.glitch.me/twitch-api/streams/' + stream) ) )
             .then ( responses => Promise.all( responses.map(response => response.json()) ) )
             .then ( json => 
@@ -18,8 +29,8 @@ function fetchStatus (...streams) {
                 console.log(resultArray); 
                 return resultArray
             } )
-            .catch ( err => { console.log(err); } );
+            .catch ( err => { console.error(err); } );
 } // returns promise object which can be used within another promise chain or evaluate by looking at PromiseValue
 
-fetchStatus('freecodecamp', 'testchannel');
+fetchStatus('freecodecamp', 'TwitchPresents');
 
