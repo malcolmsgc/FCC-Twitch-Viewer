a4a54b368d4375845b91e8b00b7e15337860450f
Malcolm Cumming <bemjogar@gmail.com>
2017-03-20 21:35:33 +0000

    refactor fetch functions into single function

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ Arguments should be strings`);
     }
 }
 
+/*
 function fetchStatus (...streams) {
     return Promise.all( streams.map( stream => fetch('https://wind-bow.glitch.me/twitch-api/streams/' + stream) ) )
             .then ( responses => Promise.all( responses.map(response => response.json()) ) )
@@ -28,7 +29,6 @@ function fetchStatus (...streams) {
             .catch ( err => { console.error(err); } );
 } // returns promise object which can be used within another promise chain or evaluate by looking at PromiseValue
 
-
 function fetchDetails (...channels) {
      return Promise.all( channels.map( channel => fetch('https://wind-bow.glitch.me/twitch-api/channels/' + channel) ) )
     .then ( responses => Promise.all( responses.map(response => response.json()) ) )
@@ -42,9 +42,19 @@ function fetchDetails (...channels) {
                 return resultArray;
             } )
     .catch ( err => { console.error(err); } );
+}*/
+
+function fetchFunc (url, refineResultsCallback , ...channels) {
+     return Promise.all( channels.map( channel => fetch(url + channel) ) )
+    .then ( responses => Promise.all( responses.map(response => response.json()) ) )
+    .then ( json =>
+        json.map( refineResultsCallback ) )
+    .then ( resultArray => {
+                console.log(resultArray); 
+                return resultArray;
+            } )
+    .catch ( err => { console.error(err); } );
 }
-
-
 
 function renderLists ({ stream , online } = {}) {
     console.log('render');
@@ -52,10 +62,28 @@ function renderLists ({ stream , online } = {}) {
 
 const channels = ['freecodecamp', 'TwitchPresents']; // add channels to include in results in this array
 validateArgs(...channels);
-Promise.all ( [fetchDetails(...channels) , fetchStatus(...channels)] )
+const fetchDetails = fetchFunc ( 
+    'https://wind-bow.glitch.me/twitch-api/channels/',
+    obj => {
+            const {display_name, logo, name, url, status} = obj;
+            return {display_name, logo, name, url, status};
+        },
+        ...channels
+        ),
+        fetchStatus = fetchFunc ( 
+            'https://wind-bow.glitch.me/twitch-api/streams/',
+            (obj, i) => { 
+                    return {
+                        stream: channels[i],
+                        online: obj['stream'] !== null
+                    }
+            },
+            ...channels           
+            );
+
+Promise.all ( [fetchDetails , fetchStatus] )
     .then ( results => {
         const [details , status] = results; //destructure results into two arrays
-
         console.log(details, status);
         renderLists();
     }
