d59e920f24cbb95e737e3e894254cbdcc1281c2f
Malcolm Cumming <bemjogar@gmail.com>
2017-04-08 19:13:02 +0100

    Fixed paths on gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,22 @@ const   gulp = require('gulp'),
         htmlclean = require('gulp-htmlclean'), //html whitespace remover
         sass = require('gulp-sass'),//sass compiler
         cssnano = require('gulp-cssnano'), //minify css
-        stripdebug = require('gulp-strip-debug'), //removes console and debug statements from js
-        uglify = require('gulp-uglify'),// js minifier
+        //stripdebug = require('gulp-strip-debug'), //removes console and debug statements from js
+        //uglify = require('gulp-uglify'),// js minifier
         useref = require('gulp-useref'), //concatenates js & css files and changes references in html to dest
         //concat = require('gulp-concat'), //concatenates js 
-        gulpIf = require('gulp-if'),
+        //gulpIf = require('gulp-if'),
         autoprefixer = require('gulp-autoprefixer'),
         newer = require('gulp-newer');
-        gulpSequence = require('gulp-sequence') //task sequencer
+        //gulpSequence = require('gulp-sequence') //task sequencer
         folder = {
             src: 'app/',
             build: 'dist/'
         }
 
 gulp.task('html', function () {
-    const   source = `${folder.src} + 'html/**/*.+(html|htm)`,
-            dest = `${folder.build} + 'html/`;
+    const   source = `${folder.src}html/**/*.+(html|htm)`,
+            dest = `${folder.build}html/`;
     return
     gulp.src(source)
     .pipe(newer(dest))
@@ -26,8 +26,8 @@ gulp.task('html', function () {
 });
 
 gulp.task('css', ['sass'], function () {
-    const   source = `${folder.src} + 'css/**/*.css`;
-            dest = `${folder.build} + 'css/`;
+    const   source = `${folder.src}css/**/*.css`;
+            dest = `${folder.build}css/`;
     return gulp.src(source)
     .pipe ( newer(dest) )
     .pipe ( autoprefixer ( {
@@ -39,10 +39,10 @@ gulp.task('css', ['sass'], function () {
 });
 
 gulp.task('sass', function () {
-    const   source = `${folder.src} + 'scss/**/*.scss`;
-            dest = `${folder.src} + 'css/`;
+    const   source = `${folder.src}scss/**/*.scss`;
+            dest = `${folder.src}css/`;
   return gulp.src(source)
-    .pipe(newer(dest))
+    .pipe( newer(dest) )
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(dest));
 });
