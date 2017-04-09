2331d19db86e33e2258ad406ca559cbac1b3f208
Malcolm Cumming <bemjogar@gmail.com>
2017-04-09 19:19:52 +0100

    gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,44 @@ const   gulp = require('gulp'),
         htmlclean = require('gulp-htmlclean'), //html whitespace remover
         sass = require('gulp-sass'),//sass compiler
         cssnano = require('gulp-cssnano'), //minify css
-        //stripdebug = require('gulp-strip-debug'), //removes console and debug statements from js
-        //uglify = require('gulp-uglify'),// js minifier
+        uglify = require('uglify-js-harmony'),// js minifier
         useref = require('gulp-useref'), //concatenates js & css files and changes references in html to dest
         //concat = require('gulp-concat'), //concatenates js 
         //gulpIf = require('gulp-if'),
         autoprefixer = require('gulp-autoprefixer'),
-        newer = require('gulp-newer');
+        newer = require('gulp-newer'),
+        //pump = require('pump');
         //gulpSequence = require('gulp-sequence') //task sequencer
         folder = {
             src: 'app/',
             build: 'dist/'
+        };
+
+gulp.task('dist', ['html', 'css', 'js'], function () {
+    return
+    gulp.src(folder.src)
+    .pipe(newer(folder.build))
+    .pipe(gulp.dest(folder.build))
+});   
+
+gulp.task('js', function () {
+    return 
+    gulp.src('app/**/*.js')
+    .pipe( uglify().on('error', function(err) {
+        console.error('Error in js compression', err.toString() )
         }
+    ))
+    .pipe( gulp.dest('dist/**/*') )
+});
 
 gulp.task('html', function () {
-    const   source = `${folder.src}html/**/*.+(html|htm)`,
+    const   source = `${folder.src}html/**/*`,
             dest = `${folder.build}html/`;
     return
     gulp.src(source)
-    .pipe(newer(dest))
-    .pipe(gulp.dest(dest))
+    .pipe( newer(dest) )
+    .pipe( htmlclean() )
+    .pipe( gulp.dest(dest) )
 });
 
 gulp.task('css', ['sass'], function () {
